11a4b804837100f332f1204f99c18bc8ba6d00a3
KSY <sykim.p508@gmail.com>
2022-06-20 21:52:05 +0900

    generate dynamic title header with react useStage HOOK!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,176 @@
-import React from "react";
-import Test from "./Test";
+import React, { useState } from "react";
+import MySvg from "./MySvg";
+import Header from "./Header";
+// import Test from "./Test";
 
 function App() {
+  const [selectedNav, setSelectedNav] = useState("Inbox");
+
+  const handleNavChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedNav(e.target.value);
+  };
+
   return (
     <>
-      <Test />
+      {/*<Test />*/}
+      <aside className="verticalFlexContainer">
+        <nav>
+          <ul className="navList">
+            <li>
+              <ul>
+                <li>
+                  <input
+                    id="inbox"
+                    type="radio"
+                    name="nav"
+                    value="Inbox"
+                    defaultChecked={true}
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="inbox">
+                    <img
+                      src={MySvg.Inbox}
+                      alt="inbox icon"
+                      className="listIcon"
+                    />
+                    Inbox
+                  </label>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li>
+                  <input
+                    id="today"
+                    type="radio"
+                    name="nav"
+                    value="Today"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="today">
+                    <img
+                      src={MySvg.Sun}
+                      alt="today icon"
+                      className="listIcon"
+                    />
+                    Today
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="upcoming"
+                    type="radio"
+                    name="nav"
+                    value="Upcoming"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="upcoming">
+                    <img
+                      src={MySvg.Calendar}
+                      alt="upcoming icon"
+                      className="listIcon"
+                    />
+                    Upcoming
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="anytime"
+                    type="radio"
+                    name="nav"
+                    value="Anytime"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="anytime">
+                    <img
+                      src={MySvg.Coffee}
+                      alt="anytime icon"
+                      className="listIcon"
+                    />
+                    Anytime
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="someday"
+                    type="radio"
+                    name="nav"
+                    value="Someday"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="someday">
+                    <img
+                      src={MySvg.BoxAlt}
+                      alt="someday icon"
+                      className="listIcon"
+                    />
+                    Someday
+                  </label>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li>
+                  <input
+                    id="logbook"
+                    type="radio"
+                    name="nav"
+                    value="Logbook"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="logbook">
+                    <img
+                      src={MySvg.Book}
+                      alt="logbook icon"
+                      className="listIcon"
+                    />
+                    Logbook
+                  </label>
+                </li>
+                <li>
+                  <input
+                    id="trash"
+                    type="radio"
+                    name="nav"
+                    value="Trash"
+                    onChange={handleNavChange}
+                  />
+                  <label htmlFor="trash">
+                    <img
+                      src={MySvg.Bin}
+                      alt="trash icon"
+                      className="listIcon"
+                    />
+                    Trash
+                  </label>
+                </li>
+              </ul>
+            </li>
+            {/* TODO: project list */}
+            {/* TODO: area and sub project list */}
+          </ul>
+        </nav>
+        <footer className="generalFooter">
+          <button>
+            <img src={MySvg.FolderAdd} alt="add project button" />
+          </button>
+          {/* TODO: setting feature */}
+          <input type="file" accept=".txt" />
+          <button>
+            <img src={MySvg.Upload} alt="upload file button" />
+          </button>
+          <button>
+            <img src={MySvg.Download} alt="export button" />
+          </button>
+        </footer>
+      </aside>
+      <section className="verticalFlexContainer">
+        <main className="verticalFlexContainer">
+          <Header nav={selectedNav} />
+        </main>
+      </section>
     </>
   );
 }
---
+++ src/MySvg.ts
@@ -0,0 +1,51 @@
+import ArrowRight from "./svg/arrow-right.svg";
+import Bin from "./svg/bin.svg";
+import Book from "./svg/book.svg";
+import BoxAlt from "./svg/box-alt.svg";
+import Calendar from "./svg/calendar.svg";
+import CalendarAdd from "./svg/calendar-add.svg";
+import Checkbox from "./svg/checkbox.svg";
+import Close from "./svg/close.svg";
+import Coffee from "./svg/coffee.svg";
+import Download from "./svg/download.svg";
+import Flag from "./svg/flag.svg";
+import FolderAdd from "./svg/folder-add.svg";
+import Inbox from "./svg/inbox.svg";
+import Layers from "./svg/layers.svg";
+import LineChart from "./svg/line-chart.svg";
+import List from "./svg/list.svg";
+import Moon from "./svg/moon.svg";
+import Plus from "./svg/plus.svg";
+import Search from "./svg/search.svg";
+import Sticker from "./svg/sticker.svg";
+import Sun from "./svg/sun.svg";
+import TagAlt from "./svg/tag-alt.svg";
+import Upload from "./svg/upload.svg";
+
+const MySvg = {
+  ArrowRight,
+  Bin,
+  Book,
+  BoxAlt,
+  Calendar,
+  CalendarAdd,
+  Checkbox,
+  Close,
+  Coffee,
+  Download,
+  Flag,
+  FolderAdd,
+  Inbox,
+  Layers,
+  LineChart,
+  List,
+  Moon,
+  Plus,
+  Search,
+  Sticker,
+  Sun,
+  TagAlt,
+  Upload,
+};
+
+export default MySvg;
